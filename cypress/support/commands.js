24673b76133c45aedf57eecef9ca27a7e92f4e34
Robert van Caem <robbertvancaem@gmail.com>
2022-08-02 13:56:30 +0200

    Increase snapshot error threshold

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -28,6 +28,7 @@ const compareSnapshotCommand = require('cypress-visual-regression/dist/command')
 
 compareSnapshotCommand({
   scale: true,
+  errorThreshold: 0.03,
 })
 
 // addMatchImageSnapshotCommand({
